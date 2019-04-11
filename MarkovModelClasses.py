9cfb36b66001123bdf7a8a5feac1ef73e6d1e7b9
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-04-11 11:59:15 -0400

    cohort initialization

--- MarkovModelClasses.py
+++ MarkovModelClasses.py
@@ -137,21 +137,23 @@ class Cohort:
         :param parameters: parameters
         """
         self.id = id
-        self.initialPopSize = pop_size  # initial population size
+        self.popSize = pop_size
+        self.params = parameters
         self.patients = []  # list of patients
         self.cohortOutcomes = CohortOutcomes()  # outcomes of the this simulated cohort
-
-        # populate the cohort
-        for i in range(pop_size):
-            # create a new patient (use id * pop_size + n as patient id)
-            patient = Patient(id=id * pop_size + i, parameters=parameters)
-            # add the patient to the cohort
-            self.patients.append(patient)
 
     def simulate(self, n_time_steps):
         """ simulate the cohort of patients over the specified number of time-steps
         :param n_time_steps: number of time steps to simulate the cohort
         """
+
+        # populate the cohort
+        for i in range(self.popSize):
+            # create a new patient (use id * pop_size + n as patient id)
+            patient = Patient(id=self.id * self.popSize + i, parameters=self.params)
+            # add the patient to the cohort
+            self.patients.append(patient)
+
         # simulate all patients
         for patient in self.patients:
             # simulate
