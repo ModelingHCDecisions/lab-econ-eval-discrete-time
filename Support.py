434de627cc467d2bd0434787ac50c6f2e91ca69d
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-04-07 10:11:59 -0400

    2020 update

--- Support.py
+++ Support.py
@@ -1,5 +1,5 @@
 import InputData as D
-import SimPy.SamplePathClasses as PathCls
+import SimPy.Plots.SamplePaths as Path
 import SimPy.Plots.Histogram as Hist
 import SimPy.StatisticalClasses as Stat
 import SimPy.EconEval as Econ
@@ -62,7 +62,7 @@ def plot_survival_curves_and_histograms(sim_outcomes_mono, sim_outcomes_combo):
     ]
 
     # graph survival curve
-    PathCls.graph_sample_paths(
+    Path.plot_sample_paths(
         sample_paths=survival_curves,
         title='Survival curve',
         x_label='Simulation time step (year)',
@@ -77,13 +77,13 @@ def plot_survival_curves_and_histograms(sim_outcomes_mono, sim_outcomes_combo):
     ]
 
     # graph histograms
-    Hist.graph_histograms(
+    Hist.plot_histograms(
         data_sets=set_of_survival_times,
         title='Histogram of patient survival time',
         x_label='Survival time (year)',
         y_label='Counts',
         bin_width=1,
-        legend=['Mono Therapy', 'Combination Therapy'],
+        legends=['Mono Therapy', 'Combination Therapy'],
         transparency=0.6
     )
 
@@ -160,13 +160,14 @@ def report_CEA_CBA(sim_outcomes_mono, sim_outcomes_combo):
     )
 
     # do CEA
+    # (the first strategy in the list of strategies is assumed to be the 'Base' strategy)
     CEA = Econ.CEA(
         strategies=[mono_therapy_strategy, combo_therapy_strategy],
         if_paired=False
     )
 
-    # show the cost-effectiveness plane
-    show_ce_figure(CEA=CEA)
+    # plot cost-effectiveness figure
+    CEA.plot_CE_plane()
 
     # report the CE table
     CEA.build_CE_table(
@@ -179,12 +180,11 @@ def report_CEA_CBA(sim_outcomes_mono, sim_outcomes_combo):
     # CBA
     NBA = Econ.CBA(
         strategies=[mono_therapy_strategy, combo_therapy_strategy],
+        wtp_range=[0, 50000],
         if_paired=False
     )
     # show the net monetary benefit figure
-    NBA.graph_incremental_NMBs(
-        min_wtp=0,
-        max_wtp=50000,
+    NBA.plot_incremental_nmbs(
         title='Cost-Benefit Analysis',
         x_label='Willingness-to-pay for one additional QALY ($)',
         y_label='Incremental Net Monetary Benefit ($)',
@@ -192,45 +192,3 @@ def report_CEA_CBA(sim_outcomes_mono, sim_outcomes_combo):
         show_legend=True,
         figure_size=(6, 5)
     )
-
-
-def show_ce_figure(CEA):
-
-    # create a cost-effectiveness plot
-    plt.figure(figsize=(5, 5))
-
-    # find the frontier (x, y)'s
-    frontier_utilities = []
-    frontier_costs = []
-    for s in CEA.get_shifted_strategies_on_frontier():
-        frontier_utilities.append(s.aveEffect)
-        frontier_costs.append(s.aveCost)
-
-    # draw the frontier line
-    plt.plot(frontier_utilities, frontier_costs,
-             c='k',  # color
-             alpha=0.6,  # transparency
-             linewidth=2,  # line width
-             label="Frontier")  # label to show in the legend
-
-    # add the strategies
-    for s in CEA.get_shifted_strategies():
-        # add the center of the cloud
-        plt.scatter(s.aveEffect, s.aveCost,
-                    c=s.color,      # color
-                    alpha=1,        # transparency
-                    marker='o',     # markers
-                    s=75,          # marker size
-                    label=s.name    # name to show in the legend
-                    )
-
-    plt.legend()        # show the legend
-    plt.axhline(y=0, c='k', linewidth=0.5)  # horizontal line at y = 0
-    plt.axvline(x=0, c='k', linewidth=0.5)  # vertical line at x = 0
-    plt.xlim([-2.5, 10])              # x-axis range
-    plt.ylim([-50000, 200000])     # y-axis range
-    plt.title('Cost-Effectiveness Analysis')
-    plt.xlabel('Additional discounted utility')
-    plt.ylabel('Additional discounted cost')
-    plt.show()
-
